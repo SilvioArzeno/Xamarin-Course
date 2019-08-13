089b17f25531f2eea3984af535ee27e075ed8e6a
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-08-13 17:21:37 -0400

    A colorful introduction to Xamarin Forms

---
+++ Week1Example/Week1Example/Week1Example/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Week1Example
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var ThisButton = (sender as Button);
+            BottomLabel.TextColor = ThisButton.BackgroundColor;
+        }
+        private void TextEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            BottomLabel.Text = TextEntry.Text;
+        }
+    }
+}
