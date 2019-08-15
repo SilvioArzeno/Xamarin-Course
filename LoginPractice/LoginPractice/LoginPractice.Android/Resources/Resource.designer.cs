6ec81e255982a21c4735550e22cec5977977a1ca
Silvio Daniel Arzeno <silviodanielarzeno@gmail.com>
2019-08-14 20:26:18 -0400

    New pink background

--- LoginPractice/LoginPractice/LoginPractice.Android/Resources/Resource.designer.cs
+++ LoginPractice/LoginPractice/LoginPractice.Android/Resources/Resource.designer.cs
@@ -6442,13 +6442,16 @@ namespace LoginPractice.Droid
 			public const int notify_panel_notification_icon_bg = 2131165497;
 			
 			// aapt resource value: 0x7F07013A
-			public const int redfade = 2131165498;
+			public const int pinkfade = 2131165498;
 			
 			// aapt resource value: 0x7F07013B
-			public const int tooltip_frame_dark = 2131165499;
+			public const int redfade = 2131165499;
 			
 			// aapt resource value: 0x7F07013C
-			public const int tooltip_frame_light = 2131165500;
+			public const int tooltip_frame_dark = 2131165500;
+			
+			// aapt resource value: 0x7F07013D
+			public const int tooltip_frame_light = 2131165501;
 			
 			static Drawable()
 			{
